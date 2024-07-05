0d0baf64e63ef3ee2ca577d047de262bf8995c92
MhdWahyudi9 <mhd.wahyudi004@mhs.mdp.ac.id>
2024-07-05 23:36:26 +0700

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,15 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Rental Mobil | @yield('title')</title>
   <!-- base:css -->
-  <link rel="stylesheet" href="{{ asset('vendors/mdi/css/materialdesignicons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css') }}">
+  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="css/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}"/>
+  <link rel="shortcut icon" href="images/favicon.png"/>
   <!-- Custom fonts for this template-->
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
